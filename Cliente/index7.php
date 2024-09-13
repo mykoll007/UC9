dc8393d079e74a669b1f058f713b0437335ef7d8
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2024-09-13 20:36:32 -0300

    metodo delete

---
+++ Cliente/index7.php
@@ -0,0 +1,22 @@
+<?php
+
+//formato post
+
+$curl = curl_init();
+curl_setopt_array($curl,array(
+    CURLOPT_URL => 'http://localhost/UC9/test_api_1/delete_client/',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    //CURLOPT_HTTP_VERSION => CURLOPT_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'DELETE',
+    CURLOPT_POSTFIELDS => '{"id":10}',
+    CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+));
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
+
+?>
