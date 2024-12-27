95ec37e4020113627debab4059b4de40ab1253f3
mykoll007 <mykoll.daniel@gmail.com>
2024-12-27 16:32:00 -0300

    personalizando api

--- API_RickAndMorty/index.php
+++ API_RickAndMorty/index.php
@@ -24,11 +24,13 @@ if ($response) {
 <head>
     <meta charset="UTF-8">
     <title>Personagens de Rick and Morty</title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="estilo.css?v=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-
+<div id="align-img">
+<img src="image-removebg-preview(1).png" alt="imagem logo rick and morty">
+</div>
 <h1>Personagens de Rick and Morty</h1>
 <ul>
     <?php if (!empty($characters)): ?>
@@ -46,5 +48,9 @@ if ($response) {
     <?php endif; ?>
 </ul>
 
+<!-- Biblioteca adicionada de particulas fundo -->
+<div id="particles-js"></div>
+
+<script src="particles.min.js"></script>
 </body>
 </html>
